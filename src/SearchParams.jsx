03a3c1f956400cc48a778fe69dd628798a6f35b0
beingamit4197 <beingamit4197@gmail.com>
2022-12-28 19:52:25 +0530

    Mapping Through Data with Hooks

--- src/SearchParams.jsx
+++ src/SearchParams.jsx
@@ -1,18 +1,41 @@
-import {useState} from "react";
+import { useState } from "react";
+
+const ANIMALS = ["Bird", "Cat", "Rabbit", "Dog"];
 
 const SearchParams = () => {
-    const [location, setLocation] = useState("");
-    return (
-        <div className="search-params">
-            <form action="">
-                <label htmlFor="location">
-                    Location
-                <input onChange={(e) => setLocation(e.target.value)} id="location" value={location} placeholder="location" />
-                </label>
-                <button>Submit</button>
-            </form>
-        </div>
-    );
+  const [location, setLocation] = useState("");
+  const [animal, setAnimal] = useState("");
+  return (
+    <div className="search-params">
+      <form action="">
+        <label htmlFor="location">
+          Location
+          <input
+            onChange={(e) => setLocation(e.target.value)}
+            id="location"
+            value={location}
+            placeholder="location"
+          />
+        </label>
+        <label htmlFor="animal">
+          Animal
+          <select
+            value="{animal}"
+            id="animal"
+            onChange={(e) => {
+              setAnimal(e.target.value);
+            }}
+          >
+            <option />
+            {ANIMALS.map((animal) => (
+              <option key={animal}>{animal}</option>
+            ))}
+          </select>
+        </label>
+        <button>Submit</button>
+      </form>
+    </div>
+  );
 };
 
-export default SearchParams; 
+export default SearchParams;
